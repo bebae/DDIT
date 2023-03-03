98f1246c214f87323aad171db9fa8a45bd2491f4
PC-03 <PC-03@DESKTOP-HCM9QA8>
2023-03-03 11:11:40 +0900

    기초

---
+++ BasicJava/src/chapter01/RunStatementExample.java
@@ -0,0 +1,11 @@
+package chapter01;
+
+public class RunStatementExample {
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 3;
+		int result = x + y;
+		System.out.println(result);
+		System.out.println("실행 확인");
+	}	
+}
