1a26d324700e3ffc7940e9dac947d00fb43efa95
PC-03 <PC-03@DESKTOP-HCM9QA8>
2023-03-06 13:06:43 +0900

    0306

--- BasicJava/src/chapter02/VariableExchangeExample.java
+++ BasicJava/src/chapter02/VariableExchangeExample.java
@@ -1,31 +1,35 @@
 package chapter02;
 
-public class VariableExchangeExample {
-	
-	static void prit(int x, int y) {
+class Tool{
+	public void prit(int x, int y) {
 		System.out.println("x:"+x+"y:"+y);
 	}
+}
 
-	
+public class VariableExchangeExample {
+		
 	public static void main(String[] args) {
+	
+		Tool t = new Tool();
+		
 		int x = 3;
 		int y = 5;
-		prit(x,y);
+		t.prit(x,y);
 		
 		int temp = x;
 		x = y;
 		y = temp;
-		prit(x, y);
+		t.prit(x, y);
 		
 		x = x+y;
 		y = x-y;
 		x = x-y;
-		prit(x, y);
+		t.prit(x, y);
 		
 		x = x+y;
 		y = x-y;
 		x = x-y;
-		prit(x, y);
+		t.prit(x, y);
 	}
 }
 
