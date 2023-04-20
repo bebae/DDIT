ef167275a31d257fd8773b436629debee428470f
BeomJin Jung <bomjn44@gmail.com>
2023-04-20 10:32:11 +0900

    04.20

--- 04.18~0/highjava/src/kr/or/ddit/basic/ListSortTest01.java
+++ 04.18~0/highjava/src/kr/or/ddit/basic/ListSortTest01.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-public class ListSortTest {
+public class ListSortTest01 {
     public static void main(String[] args) {
         ArrayList<String> list = new ArrayList<String>();
         list.add("일지매");
